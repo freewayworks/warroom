8a3be7e1845bb9319176a55994d404fa2813003f
David Asabina <david@supr.nu>
2015-08-27 16:53:27 +0200

    Show freeway-taxi (aka mvp) app version

--- jobs/heroku_releases.rb
+++ jobs/heroku_releases.rb
@@ -1,16 +1,20 @@
 require 'curb'
 
 SCHEDULER.every '1h', first_in: 0 do
-  res = Curl.get('https://api.heroku.com/apps/add-campaign/releases') do |http|
-    http.headers['Accept'] = 'application/vnd.heroku+json; version=3'
-    http.headers['Authorization'] = "Bearer #{ENV['HEROKU_API_KEY']}"
-    puts http.headers
-  end
+  heroku_apps = ['add-campaign', 'freeway-taxi']
 
-  json_result = JSON.parse(res.body_str)
-  last_version = json_result.last["version"]
-  version_count = json_result.count
-  puts "version: #{last_version}"
-  send_event('dashboard_last_version', { current: last_version })
-  send_event('dashboard_version_count', { current: version_count })
+  heroku_apps.map do |app_name|
+    res = Curl.get("https://api.heroku.com/apps/#{app_name}/releases") do |http|
+      http.headers['Accept'] = 'application/vnd.heroku+json; version=3'
+      http.headers['Authorization'] = "Bearer #{ENV['HEROKU_API_KEY']}"
+      puts http.headers
+    end
+  
+    json_result = JSON.parse(res.body_str)
+    last_version = json_result.last["version"]
+    version_count = json_result.count
+    puts "#{app_name} version: #{last_version}"
+    send_event("#{app_name}_last_version", { current: last_version })
+    send_event("#{app_name}_version_count", { current: version_count })
+  end
 end
